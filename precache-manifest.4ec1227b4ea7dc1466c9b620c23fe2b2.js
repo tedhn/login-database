a3184bea41ae9a58c07390b943df92dbcb2931d7
ted <heinhtetnaing186@gmail.com>
2020-08-11 16:13:14 +0630

    Updates

---
+++ precache-manifest.4ec1227b4ea7dc1466c9b620c23fe2b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3dd6dcd24fc955d459a7bac81fcd104",
+    "url": "/login-database/index.html"
+  },
+  {
+    "revision": "62772d3d7c0fd904d4e3",
+    "url": "/login-database/static/css/main.b7df239b.chunk.css"
+  },
+  {
+    "revision": "898d1c69775dcaea99b0",
+    "url": "/login-database/static/js/2.7df4187b.chunk.js"
+  },
+  {
+    "revision": "7026b92093b6c9d3661178c162f9c097",
+    "url": "/login-database/static/js/2.7df4187b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62772d3d7c0fd904d4e3",
+    "url": "/login-database/static/js/main.37f716ad.chunk.js"
+  },
+  {
+    "revision": "74129ca771daa3b391eb",
+    "url": "/login-database/static/js/runtime-main.193a62f1.js"
+  },
+  {
+    "revision": "db823ae99ab7eea513362e59c0989215",
+    "url": "/login-database/static/media/bg.db823ae9.jpg"
+  },
+  {
+    "revision": "a01a993ac8abc06ee0c949838f9cb109",
+    "url": "/login-database/static/media/firebase.a01a993a.js~"
+  }
+]);
